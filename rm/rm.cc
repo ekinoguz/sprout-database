51f1dad21fb0bef8fb0bb0432e9c3e8edd11b0c4
Sky Faber <sky.faber@gmail.com>
2013-04-17 15:58:43 -0700

    Added tests

--- rm/rm.cc
+++ rm/rm.cc
@@ -19,3 +19,59 @@ RM::~RM()
 {
 }
 
+
+RC RM::createTable(const string tableName, const vector<Attribute> &attrs)
+{
+
+  return 0;
+}
+
+RC RM::deleteTable(const string tableName)
+{
+  return 0;
+}
+
+RC RM::getAttributes(const string tableName, vector<Attribute> &attrs)
+{
+  return 0;
+}
+RC RM::insertTuple(const string tableName, const void *data, RID &rid)
+{
+  return 0;
+}
+
+
+RC RM::deleteTuples(const string tableName)
+{
+  return 0;
+}
+RC RM::deleteTuple(const string tableName, const RID &rid)
+{
+  return 0;
+}
+// Assume the rid does not change after update
+RC RM::updateTuple(const string tableName, const void *data, const RID &rid)
+{
+  return 0;
+}
+RC RM::readTuple(const string tableName, const RID &rid, void *data)
+{
+  return 0;
+}
+RC RM::readAttribute(const string tableName, const RID &rid, const string attributeName, void *data)
+{
+  return 0;
+}
+RC RM::reorganizePage(const string tableName, const unsigned pageNumber)
+{
+  return 0;
+}
+RC RM::scan(const string tableName,
+      const string conditionAttribute,
+      const CompOp compOp,                  // comparision type such as "<" and "="
+      const void *value,                    // used in the comparison
+      const vector<string> &attributeNames, // a list of projected attributes
+      RM_ScanIterator &rm_ScanIterator)
+{
+  return 0;
+}
