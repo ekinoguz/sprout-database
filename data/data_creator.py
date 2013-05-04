d5c0bb29953f1829d106b224e1fb2263661eb361
ekinoguz <ekinoguz@gmail.com>
2013-05-04 16:06:22 -0700

    adding more test data

--- data/data_creator.py
+++ data/data_creator.py
@@ -12,9 +12,10 @@ def employee():
 		age = random.randint(18,70)
 		height = random.randint(55, 71)/10.0
 		salary = random.randint(50, 250) * 1008
-		out += line[:-1] + ',' + str(age) + ',' + str(height)  + ',' + str(salary) + '\n'
+		#out += line[:-1] + ',' + str(age) + ',' + str(height)  + ',' + str(salary) + '\n'
+		out += line[:-1] + ',' + str(age) + ',' + str(height)  + '\n'
 	f.close()
-	f = open('employee', 'w')
+	f = open('employee_50_no_salary', 'w')
 	f.write(out)
 	print out
 
