bf40f5214b77919afcda03ca841292dd3547d9b6
ekinoguz <ekinoguz@gmail.com>
2013-05-03 22:15:09 -0700

    fixing bugs from last commit

--- cli/cli.cc
+++ cli/cli.cc
@@ -25,13 +25,8 @@ CLI::CLI()
   rm = RM::Instance();
   Attribute attr;
 
-<<<<<<< HEAD
-	// create cli columns table
-	vector<Attribute> column_attrs;
-=======
   // create cli columns table
   vector<Attribute> column_attrs;
->>>>>>> a41b41e57ee772b35b8c58e659ba6c73c6d21994
   attr.name = "column_name";
   attr.type = TypeVarChar;
   attr.length = 30;
@@ -611,7 +606,6 @@ void CLI::error(const string errorMessage)
 {
 	cout << errorMessage << endl;
 }
-<<<<<<< HEAD
 
 void CLI::printAttributes(vector<Attribute> &attributes)
 {
@@ -619,5 +613,3 @@ void CLI::printAttributes(vector<Attribute> &attributes)
 		cout << setw(it->length+5) << left << it->name;
 	cout << endl;
 }
-=======
->>>>>>> a41b41e57ee772b35b8c58e659ba6c73c6d21994
