e64c05b93750249d854a01f0dced9d1d3466afd3
ekinoguz <ekinoguz@gmail.com>
2013-05-22 12:10:15 -0700

    start.cc is added to start cli without deleting databases

---
+++ cli/start.cc
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cassert>
+
+#include "cli.h"
+
+using namespace std;
+
+CLI *cli;
+
+int main()
+{
+  //system("rm -r \"" DATABASE_FOLDER "\" 2> /dev/null");
+  
+  cli = CLI::Instance();
+  cli->start();
+
+  return 0;
+}
